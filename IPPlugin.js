f7233386035cd600fad8cf4e09a4085cd38632ff
Devil <853695231@qq.com>
2016-10-07 10:45:10 +0800

    新修改的内容

--- IPPlugin.js
+++ IPPlugin.js
@@ -4,124 +4,100 @@ $.fn.selfsettings={
 		  	 parent_css:{},//当前元素的CSS样式
 	         input_css:{"font-size":"20px","ime-mode":"disabled"},//生成的每个input标签的样式
 	         xhforID:[],//生成的每个Input标签的ID属性数字
-	         ipaddress:""//默认的IP数据 
+	         defaultvalue:"",//默认的IP数据 
+	         prefix:"ipdz",//所生成标签的英文名称前缀
+	         total_input_cnt:4,//所有的input标签总数
+	         usedfor:"ip",//使用该插件的用途，用于区别IP和MAC的用途。
+	         splitletter:"."//调用getValue方法返回值的分隔符 。
 };
 //插件初始化的默认方法 
 $.fn.ipAddress=function(option)
 {
-	bind_UUID_Math();
-	$.fn.selfsettings.xhforID=new Array();
-	for(var index=0;index<4;index++)
+	bind_UUID_Math();//为math对象绑定生成UUID的方法。
+	this.selfsettings=$.extend({}, $.fn.selfsettings,option);//合并用户配置的各项参数。
+	if(this.selfsettings.xhforID.length==0)//如果用户没有配置input标签的ID数组就生成相应的Input标签
 		{
-			$.fn.selfsettings.xhforID.push(Math.uuidFast());
+			this.selfsettings.xhforID=new Array();//定义存放UUID生成的ID的数组
+			for(var index=0;index<this.selfsettings.total_input_cnt;index++)//根据要生成input标签的总个数开始生成。
+			{
+				this.selfsettings.xhforID.push(this.selfsettings.prefix+Math.uuidFast());//将生成的UUID存放到数组当中。
+			}
 		}
-	 this.selfsettings=$.extend({}, $.fn.selfsettings,option);
-     $(this).data("myself",this);
-     genIPinput(this);
-     return this;
+     genIPinput(this);//根据当前用户配置的参数生成Input标签框。
+     return this;//返回当前对象
 };
+//该方法是用于返回生成UUID值即各项ID值
 $.fn.getCurrentIDS=function()
 {
-	return this.selfsettings.xhforID;
+	return this.selfsettings.xhforID;//返回ID数组
 };
-$.fn.setIPValue=function(ipval)
+$.fn.setValue=function(value)
 {
-	var xharr=$(this).data("myself").selfsettings.xhforID;
-	var iparr=ipval.split(".");
-	$(xharr).each(function(index,iditem){
-		$("#ipdz"+iditem).val(iparr[index]);
+	//获取当前对象的所有input标签的ID数组(当前的对象一般是不同的一个组内的input标签)
+	var xharr=this.selfsettings.xhforID;
+	var arr=null;//定义切割用到的数组
+	if(this.selfsettings.usedfor=="ip")//判断是否为用来作IP使用
+		arr=value.split(".");//对当前的传入的IP地址进行切割
+	else
+		arr=value.split(":");//被用来作为MAC使用
+	//遍历ID数组进行赋值
+	$(xharr).each(function(index,iditem){//遍历切割的数组
+		$("#"+iditem).val(arr[index]);//放到对应的input标签当中
 	});
 };
-//对象方法，返回当前对象的IP值 
-$.fn.getIPValue=function(){
-     var ipresult=""; 
-     var xharr=$(this).data("myself").selfsettings.xhforID;
-     $(xharr).each(function(index,ip){
-    	if($("#ipdz"+ip).val().length==0)
+//对象方法，返回当前对象的IP值 或者MAC地址
+$.fn.getValue=function(){
+	 var self=this;//获取当前调用对象
+     var ipresult=""; //定义返回结果的变量
+     var usedfor=self.selfsettings.usedfor;//获取当前调用对象的用途
+     $(self.selfsettings.xhforID).each(function(index,iditem){
+    	if($("#"+iditem).val().length==0)
     		{
-				alert("请输入第"+(index+1)+"项的IP内容！");
-				ipresult="";
+    			if(usedfor=="ip")
+    				alert("请输入第"+(index+1)+"项的IP地址！");
+    			else
+    				alert("请输入第"+(index+1)+"项的MAC地址！");
+				ipresult=null;
 				return false;
     		}
     	else
-    			index==xharr.length-1?ipresult+=$("#ipdz"+ip).val():ipresult+=$("#ipdz"+ip).val()+".";
+    			index==self.selfsettings.xhforID.length-1?ipresult+=$("#"+iditem).val():ipresult+=$("#"+iditem).val()+self.selfsettings.splitletter;
     });
     return ipresult;
 };
-//向目标元素当中追加4个IP对应的input标签。
+//向目标元素当中追加n个IP对应的input标签。
 function genIPinput(targetarea)
 {
-	 //获取用户配置的IP地址，并利用.进行切割
-	 var iparr=$(targetarea).data("myself").selfsettings.ipaddress.split(".");
 	 //获取生成input标签的ID属性序号
-	 var xharr=$(targetarea).data("myself").selfsettings.xhforID;
+	 var xharr=targetarea.selfsettings.xhforID;
 	 //设置当前对象的样式
-	 $(targetarea).css($(targetarea).data("myself").selfsettings.parent_css);
+	 $(targetarea).css(targetarea.selfsettings.parent_css);
 	 //获取给IP标签所设置的样式
-	 var ipinput_css=$(targetarea).data("myself").selfsettings.input_css;
+	 var ipinput_css=targetarea.selfsettings.input_css;
 	 //遍历ID序号数组生成每个input标签
     $(xharr).each(function(index,item)
     		{
 	             var $IP=$("<input type='text'>");
+	             $IP.get(0).selfsettings=targetarea.selfsettings;
+	             $IP.get(0).setValue=targetarea.setValue;
 	             $IP.attr(
 	            		 {
-	            			 "id":"ipdz"+item,
-	            			 "numid":item,
-	            			 "isfirst":index==0?"true":"false"
+	            			 "id":item,
+	            			 "numid":index,
+	            			 "isfirst":index==0?"true":"false",
+	            			 "next":targetarea.selfsettings.xhforID[index+1]==undefined?"":targetarea.selfsettings.xhforID[index+1]
 	             		 })
-	             	.val(function(){
-	             		return iparr[index]==undefined? "" : iparr[index];
-	             	})
 	             	.css(ipinput_css)
 	                .keyup(function(event){
-	                	var cur_parent=$(this).parent().data("myself");
-	                	var self=this;
-	                	var xhforID=cur_parent.selfsettings.xhforID;
-	                	var id = $(self).attr("id");
-	                	var value = parseInt($(self).val());
+	                	if(event.target.selfsettings.usedfor=="ip")
+	                		usedForIP(event)
+	                	else
+	                		usedForMAC(event)
+	                })
+	                .keydown(function(event){
 	                	var code = event.keyCode; // 当前输入的键盘值
-	                	if ((code >= 48 && code <= 57) || (code >= 96 && code <= 105)) {
-	                			if ("ipdz"+xhforID[0] == id) 
-	                			{
-	                				if (value > 233) {
-	                					alert(value + " 不是有效项，请指定一个1和233之间的值。");
-	                					$(self).val("");
-	                				} else {
-	                					if ($(self).val().length == 3) { // 三位合法数字时，当光标在ipdz1时，跳到ipdz2
-	                						$("#ipdz"+xhforID[1]).focus();
-	                					}
-	                				}
-	                			} 
-	                			else if ("ipdz"+xhforID[1] == id || "ipdz"+xhforID[2] == id || "ipdz"+xhforID[3] == id) {
-	                				if (value > 255)
-	                				{
-	                					alert(value + " 不是有效项，请指定一个1和255之间的值。");
-	                					$(self).val("");
-	                				}
-	                				else
-	                				{
-	                					if ($(self).val().length == 3)
-	                					{ // 三位合法数字时
-	                						if ("ipdz"+xhforID[1] == id) 
-	                						{ // 当光标在ipdz2时，跳到ipdz3
-	                							$("#ipdz"+xhforID[2]).focus();
-	                						} else if ("ipdz"+xhforID[2] == id) 
-	                						{ // 当光标在ipdz3时，跳到ipdz4
-	                							$("#ipdz"+xhforID[3]).focus();
-	                						}
-	                					}
-	                				}
-	                			}
-	                		}
-	                }).
-	                keydown(function(event){
-	                	var cur_parent=$(this).parent().data("myself");
 	                	var self=this;
-	                	var xhforID=cur_parent.selfsettings.xhforID;;
-	                	var id = $(self).attr("id"); // 当前输入框的ID
-	            		var code = event.keyCode; // 当前输入的键盘值
-	            		// 除了数字键、删除键、小数点之外全部不允许输入
-	            		if (code == 8) 
+	                	if (code == 8) 
 	            		{ // 删除键
 	            			if($(self).val().length==0)
 	            				{
@@ -129,48 +105,60 @@ function genIPinput(targetarea)
 	            					$(self).prev().focus().select();
 	            				}
 	            			return true;
-	            		} else if ((code >= 48 && code <= 57) || (code >= 96 && code <= 105)) 
-	            		{ // 数字键
-	            			return true;
-	            		} else if (code == 110 || code == 190) 
-	            		{// 110、190代表键盘上的两个点
-	            			if ("ipdz"+xhforID[0] == id) { // 如果是第一个框则第二个框获的焦点
-	            				$("#ipdz"+xhforID[1]).focus();
-	            			} else if ("ipdz"+xhforID[1] == id) { // 如果是第二个框则第三个框获的焦点
-	            				$("#ipdz"+xhforID[2]).focus();
-	            			} else if ("ipdz"+xhforID[2] == id) { // 如果是第三个框则第四个框获的焦点
-	            				$("#ipdz"+xhforID[3]).focus();
-	            			}
-	            			return false;
-	            		} else if (code == 37) { // 37:Left Arrow
-	            			if ("ipdz"+xhforID[1] == id) {
-	            				$("#ipdz"+xhforID[0]).focus();
-	            			} else if ("ipdz"+xhforID[2] == id) {
-	            				$("#ipdz"+xhforID[1]).focus();
-	            			} else if ("ipdz"+xhforID[3] == id) {
-	            				$("#ipdz"+xhforID[2]).focus();
-	            			}
-	            			return false;
-	            		} else if (code == 39) { // 39:Right Arrow
-	            			if ("ipdz"+xhforID[0] == id) {
-	            				$("#ipdz"+xhforID[1]).focus();
-	            			} else if ("ipdz"+xhforID[1] == id) {
-	            				$("#ipdz"+xhforID[2]).focus();
-	            			} else if ("ipdz"+xhforID[2] == id) {
-	            				$("#ipdz"+xhforID[3]).focus();
-	            			}
-	            		} else {
-	            			return false;
 	            		}
+	                	if(event.target.selfsettings.usedfor=="ip")
+	                		{
+			                	var xhforID=this.selfsettings.xhforID;;
+			                	var id = $(self).attr("id"); // 当前输入框的ID
+			            		// 除了数字键、删除键、小数点之外全部不允许输入
+			            		if (code == 8) 
+			            		{ // 删除键
+			            			if($(self).val().length==0)
+			            				{
+			            				if($(self).attr("isfirst")=="false")	
+			            					$(self).prev().focus().select();
+			            				}
+			            			return true;
+			            		} else if ((code >= 48 && code <= 57) || (code >= 96 && code <= 105)) 
+			            		{ // 数字键
+			            			return true;
+			            		} else if (code == 110 || code == 190 || code==9)//9为tab箭 
+			            		{// 110、190代表键盘上的两个点
+			            			var next=$(this).attr("next");
+			            			$("#"+next).focus().select();
+			            			return false;
+			            		} else if (code == 37 ) { // 37:左箭头
+			            			return true;
+			            		} else if (code == 39) { // 39:右箭头
+			            			return true;
+			            		} else {
+			            			return false;
+			            		}
+	                	}
+	                else
+	                	{
+	                		if(code==9)
+	                			return false;
+	                		return true;
+	                	}
 	                })
 	                .on("paste",function(event){
 	                	var target=event.target;
 	                	setTimeout(function(){
-	                		$(target).parent().data("myself").setIPValue(target.value);
+	                		target.setValue(target.value);
 	                	},10);
+	                })
+	                .on("blur",function(event){
+	                	if(this.selfsettings.usedfor=="mac" && this.value.length<2)
+	                		{
+	                			var id="#"+$(this).attr("id");
+	                			$(id).select();
+	                		}
 	                })
 	                .appendTo(targetarea);
     		});
+    if(targetarea.selfsettings.ipaddress != "")
+    	targetarea.setValue(targetarea.selfsettings.defaultvalue);//获取用户配置的IP地址，并利用.进行切割
  };
  function bind_UUID_Math() {
 	 var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
@@ -230,4 +218,68 @@ function genIPinput(targetarea)
 	    });
 	  };
 }
+ function usedForIP(eventtarget)
+ {
+	var target=self=eventtarget.target;
+ 	var xhforID=self.selfsettings.xhforID;
+ 	var id = $(self).attr("id");
+ 	var value = parseInt($(self).val());
+ 	var code = eventtarget.keyCode; // 当前输入的键盘值
+	 if ((code >= 48 && code <= 57) || (code >= 96 && code <= 105))
+ 	{
+ 			if (xhforID[0] == id) 
+ 			{
+ 				if (value > 233) {
+ 					alert(value + " 不是有效项，请指定一个1和233之间的值。");
+ 					$(self).val("");
+ 				} else {
+ 					if ($(self).val().length == 3) { // 三位合法数字时，当光标在ipdz1时，跳到ipdz2
+ 						$("#"+xhforID[1]).focus();
+ 					}
+ 				}
+ 			} 
+ 			else if (xhforID[1] == id || xhforID[2] == id || xhforID[3] == id) {
+ 				if (value > 255)
+ 				{
+ 					alert(value + " 不是有效项，请指定一个1和255之间的值。");
+ 					$(self).val("");
+ 				}
+ 				else
+ 				{
+ 					if ($(self).val().length == 3)
+ 					{ // 三位合法数字
+ 						var next=$(self).attr("next");
+	            			$("#"+next).focus().select();
+ 					}
+ 				}
+ 			}
+ 		}
+ };
+ function usedForMAC(eventtarget)
+ {
+	  var input_target=eventtarget.target;
+	  input_target.value=input_target.value.toUpperCase();
+		if(input_target.value.length==2)
+			{
+				var patternstr=/[0-9a-fA-F]{2}/g;
+				if(input_target.value.search(patternstr)!=-1)
+				{
+					var idvalue=$(input_target).attr("next");
+					$("#"+idvalue).focus().select();
+				}
+				else
+				{
+					alert("MAC地址输入有误");
+					$(input_target).val("");
+				}
+			}
+		else
+			{
+				if(input_target.value.length>2)
+					{
+						alert("MAC地址位数超出范围！");
+						$(input_target).val("")
+					}
+			}
+ };
 })(jQuery);
